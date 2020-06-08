8a2efffb5ede5d2968a2cd50d5c1029d28350f95
Kateryna <k.avramenko111@gmail.com>
2020-06-09 00:48:56 +0300

    Ready (maybe) for heroku.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware').createProxyMiddleware;
+
+const apiProxy = proxy('/api',
+    {
+        target: 'https://task-schedule-manager.herokuapp.com',
+        changeOrigin: true
+    });
+
+const app = express();
+
+app.use(apiProxy);
+
+app.use(express.static('./dist/task-schedule-manager'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/task-schedule-manager/'}),
+);
+
+app.listen(process.env.PORT || 8080);
